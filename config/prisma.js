e0ffaec65ff66e9ac8945f75d93a569df368e64a
chxmek <Chawanwits.mek@gmail.com>
2024-10-13 14:14:19 +0700

    check db is connected

--- config/prisma.js
+++ config/prisma.js
@@ -1,4 +1,18 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+async function connectToDatabase() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully");
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  } finally {
+    // await prisma.$disconnect();
+    // console.log("Disconnected from the database");
+  }
+}
+
+connectToDatabase();
+
 module.exports = prisma;
