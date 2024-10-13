5e5bd73d3bc68b76632949278e5a9e1ec56766b1
chxmek <Chawanwits.mek@gmail.com>
2024-10-13 13:20:32 +0700

    create register system

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,40 @@
+const prisma = require("../config/prisma");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
 exports.register = async (req, res) => {
   try {
     const { email, password } = req.body;
+
+    // 1. validate body
     if (!email) {
       return res.status(400).json({ message: "Email is required" });
     }
     if (!password) {
-        return res.status(400).json({ message: "Password is required"})
+      return res.status(400).json({ message: "Password is required" });
     }
-    res.send("Hello Register");
+
+    // 2. check email in db
+    const user = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (user) {
+      return res.status(400).json({ message: "Email already exits" });
+    }
+
+    // 3. hash password
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    // 4. register
+    await prisma.user.create({
+      data: {
+        email: email,
+        password: hashPassword,
+      },
+    });
+    res.send("Register success");
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Server error" });
