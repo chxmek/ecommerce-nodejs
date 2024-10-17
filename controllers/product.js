daef47f85a7fa2c47d727d04f129aa0d1b06a7cb
chxmek <Chawanwits.mek@gmail.com>
2024-10-17 18:10:50 +0700

    add create product

--- controllers/product.js
+++ controllers/product.js
@@ -2,17 +2,73 @@ const prisma = require("../config/prisma");
 
 exports.create = async (req, res) => {
   try {
-    const { title, description, price } = req.body;
+    const { title, description, price, quantity, categoryId, images } =
+      req.body;
     const product = await prisma.product.create({
-      where: {
+      data: {
         title: title,
         description: description,
-        price: price,
-        quantity: quantity,
-        categoryId: categoryId,
-        images: images,
+        price: parseFloat(price),
+        quantity: parseInt(quantity),
+        categoryId: parseInt(categoryId),
+        images: {
+          create: images.map((item) => ({
+            asset_id: item.asset_id,
+            public_id: item.public_id,
+            url: item.url,
+            secure_url: item.secure_url,
+          })),
+        },
       },
     });
+    res.send(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.list = async (req, res) => {
+  try {
+    const product = await prisma.product.findMany();
+    res.send(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    // const product = await prisma.product.findMany();
+    res.send(product);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.remove = async (req, res) => {
+  try {
+    res.send("Remove");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.listBy = async (req, res) => {
+  try {
+    res.send("listBy");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.searchFilters = async (req, res) => {
+  try {
+    const product = await prisma.product.findMany();
     res.send(product);
   } catch (err) {
     console.log(err);
--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const {
+  create,
+  list,
+  remove,
+  listBy,
+  searchFilters,
+  update,
+} = require("../controllers/product");
 const router = express.Router();
 
-router.post("/product",);
-router.get("/product/:id",);
-router.delete("/product/:id",);
-router.post("/productby",);
-router.post("/search/filters",);
+router.post("/product", create);
+router.get("/product/:count", list);
+router.put("/product/:id", update);
+router.delete("/product/:id", remove);
+router.post("/productby", listBy);
+router.post("/search/filters", searchFilters);
 
 module.exports = router;
